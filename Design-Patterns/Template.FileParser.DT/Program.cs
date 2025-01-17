0ffb27c1519443dc82e6d9f45d5ad090105c6bc4
Alex <alexandru.i.jurju@gmail.com>
2025-01-17 12:00:53 +0200

    behavioral - template

---
+++ Design-Patterns/Template.FileParser.DT/Program.cs
@@ -0,0 +1,20 @@
+﻿using Template.FileParser.DT;
+
+FileParser csvParser = new CsvParser();
+FileParser jsonParser = new JsonParser();
+
+var csvData = csvParser.ParseFile(@".\config.csv");
+foreach (var pair in csvData)
+{
+    Console.WriteLine($"{pair.Key}: {pair.Value}");
+}
+
+Console.WriteLine();
+
+var jsonData = jsonParser.ParseFile(@".\config.json");
+foreach (var pair in jsonData)
+{
+    Console.WriteLine($"{pair.Key}: {pair.Value}");
+}
+
+Console.WriteLine();
---
+++ Design-Patterns/Template.Template.DT/Program.cs
@@ -0,0 +1,8 @@
+﻿using Template.Template.DT;
+
+Template.Template.DT.Template instance = new ConcreteAlgorithmA();
+instance.TemplateMethod();
+Console.WriteLine();
+
+instance = new ConcreteAlgorithmB();
+instance.TemplateMethod();
